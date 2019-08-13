4541405285b28ed59689596ad050df62807a0007
Damodar Lohani <dlohani48@gmail.com>
2019-08-13 06:35:56 +0545

    theme color

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Fire Chat',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MainScreen(),
     );
